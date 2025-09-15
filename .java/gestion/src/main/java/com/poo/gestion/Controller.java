728d50d1a891bdf2373a7dc1d4593f2d5b35896d
Amado <sag25257@uvg.edu.gt>
2025-09-14 19:26:10 -0600

    Controller rename

--- .java/gestion/src/main/java/com/poo/gestion/Controller.java
+++ .java/gestion/src/main/java/com/poo/gestion/Controller.java
@@ -4,22 +4,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-public class HelloController {
+public class Controller {
     @CrossOrigin
     @PostMapping("/api/sign-up")
     public String signUp(@RequestBody SignUp signUp) {
         SignUp user = signUp;
         return user.crearCuenta();
     }
-
-    // GET, POST, PUT/PATCH, DELETE
-    @GetMapping("/api/hello")
-    public String getMethodName() {
-        return "Hello, world! ";
-    }
-
 }
