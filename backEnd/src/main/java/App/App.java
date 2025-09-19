22a53c1f6d0108f7efc3563935af8200c724808e
Oscarro11 <oscarrodrigomelchore@gmail.com>
2025-09-18 18:40:50 -0600

    feat: springboot java framework added, using gradle

--- backEnd/src/main/java/App/App.java
+++
@@ -1,7 +0,0 @@
-package main.java.App;
-
-public class App{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
---
+++ backEnd/src/main/java/com/example/demo/dto/MessageDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+public class MessageDTO {
+    private String text;
+
+    public MessageDTO(){}
+    
+    public String getText() {return text;}
+
+    public void setText(String text) {this.text = text;}
+}
