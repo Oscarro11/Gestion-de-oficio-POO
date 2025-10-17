fc1f6786407a8a1df74380c5235fa00ca2f5f9ee
Jimena <mjimevm@gmail.com>
2025-10-16 18:03:54 -0600

    Added navbar to dashboard

--- frontEnd/navbar/navbarManager.js
+++ frontEnd/navbar/navbarManager.js
@@ -6,9 +6,9 @@ class NavbarManager extends HTMLElement {
 
   connectedCallback() {
     this.shadowRoot.innerHTML = `
-      <link rel="stylesheet" href="navbar.css">
+      <link rel="stylesheet" href="../navbar/navbar.css">
       <div class="nav">
-        <img src="Logo.png" alt="Logo" class="logo">
+        <img src="../assets/images/Logo.png" alt="Logo" class="logo">
         <input type="checkbox" id="nav-check">
         <div class="nav-header"></div>
         <div class="nav-btn">
@@ -19,10 +19,10 @@ class NavbarManager extends HTMLElement {
           </label>
         </div>
         <div class="nav-links">
-          <a href="dashboard.html">Inicio</a>
+          <a href="../../pages/Inicio/dashboard.html">Inicio</a>
           <a href="profile.html">Perfil</a>
           <a href="settings.html">Configuración</a>
-          <a href="landing.html">Cerrar Sesión</a>
+          <a href="../../pages/Bienvenida/index.html">Cerrar Sesión</a>
         </div>
       </div>
     `;
