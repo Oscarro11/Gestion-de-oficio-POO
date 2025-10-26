f7663d40b62273013c66262f8d28d1cfa78f0402
Oscarro11 <oscarrodrigomelchore@gmail.com>
2025-10-26 17:43:09 -0600

    chore(Models): removed unnecessary imports from models

--- backEnd/src/main/java/com/example/demo/model/User.java
+++ backEnd/src/main/java/com/example/demo/model/User.java
@@ -1,20 +1,16 @@
 package com.example.demo.model;
 
-import java.util.List;
-import java.util.ArrayList;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+
+import java.util.List;
 
 @Entity
 @Table(name = "USERS")
--- backEnd/src/main/java/com/example/demo/model/Worker.java
+++ backEnd/src/main/java/com/example/demo/model/Worker.java
@@ -1,6 +1,5 @@
 package com.example.demo.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -9,14 +8,10 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-//import jakarta.persistence.OneToMany;
-//import jakarta.persistence.JoinColumn;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
-import java.util.ArrayList;
 import java.util.List; 
   
 @Entity
